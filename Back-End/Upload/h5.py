51cd05309a22138daa917b2b66fe2267a8db2eda
Afariphi <agung.afr10@gmail.com>
2023-12-16 16:58:57 +0700

    major update

--- Back-End/Upload/h5.py
+++ Back-End/Upload/h5.py
@@ -7,6 +7,9 @@ from tensorflow.keras.applications.inception_v3 import preprocess_input
 import numpy as np
 import mysql.connector
 from mysql.connector import Error
+from dotenv import load_dotenv
+
+load_dotenv()
 
 model = load_model('Model.h5')
 def prepare_image(img_path):
@@ -20,10 +23,10 @@ def create_connection():
     connection = None
     try:
         connection = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="",
-            database="catora"
+            host=os.getenv("HOST"),
+            user=os.getenv("USER"),
+            password=os.getenv("PASSWORD"),
+            database=os.getenv("DATABASE")
         )
         print("Connected to MySQL database!")
     except Error as e:
