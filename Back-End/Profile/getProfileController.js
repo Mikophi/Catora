fd887854c601debbc2d559f1e69563112e3eb11d
Afariphi <agung.afr10@gmail.com>
2023-12-16 16:11:46 +0700

    adw

--- Back-End/Profile/getProfileController.js
+++ Back-End/Profile/getProfileController.js
@@ -4,7 +4,6 @@ const db = require('../connection');
 const { resolve } = require('url');
 const path = require('path');
 
-router.use('/images', express.static(path.join(__dirname, '../Back-End/Profile/Image')));
 
 router.get('/profile/:user_id', (req, res) => {
   const { user_id } = req.params;
@@ -21,14 +20,16 @@ router.get('/profile/:user_id', (req, res) => {
       return res.status(404).json({ error: 'Profile not found' });
     }
 
+    // console.log(getProfileQuery);
+
     const userProfile = results[0];
 
     userProfile.profile_image_url = userProfile.profile_image_url
-      ? resolve('http://localhost:3000/images', userProfile.profile_image_url)
+      ? resolve('http://localhost:3000/profile/images/', path.basename(userProfile.profile_image_url))
       : null;
 
     userProfile.background_image_url = userProfile.background_image_url
-      ? resolve('http://localhost:3000/images', userProfile.background_image_url)
+      ? resolve('http://localhost:3000/profile/images/', path.basename(userProfile.background_image_url))
       : null;
 
     res.status(200).json(userProfile);
--- Back-End/Profile/profileController.js
+++ Back-End/Profile/profileController.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const getProfileController = require('./getProfileController');
 const updateProfileController = require('./updateProfileController');
+const getImage = require('./getImage');
 
 router.get('/profile/:user_id', getProfileController);
 router.put('/profile/:user_id', updateProfileController);
+router.use('/', getImage);
 
 module.exports = router;
